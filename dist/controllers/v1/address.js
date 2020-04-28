4dc08a6c4779ed76b89039a908009d50e706cb88
ming <matwming114@gmail.com>
2020-04-28 12:57:51 +1000

    fixed save address api

--- dist/controllers/v1/address.js
+++ dist/controllers/v1/address.js
@@ -25,13 +25,21 @@ exports.saveAddress = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
     const { charge_code, deliver_to, country, residence, address, phone, consignment_weight, product_classification, expected_dispatch, contents, unit_value, value, } = req.body;
-    console.log("charge_code", charge_code, "country", country, "deliver_to", deliver_to);
+    console.log("expected_dispatch", expected_dispatch);
     app_1.pool.query(`insert into shippments (charge_code,deliver_to,country,residence,detail_address,phone,consignment_weight,product_id,expected_dispatch,contents,unit_value,value) 
          values("${charge_code}","${deliver_to}","${country}","${residence}","${address}","${phone}","${consignment_weight}","${product_classification}","${expected_dispatch}","${contents}","${unit_value}","${value}")`, async (err, result, fields) => {
         if (err) {
-            console.log('insert into shippments has errors', err);
+            console.log("insert into shippments has errors", err);
             return;
         }
         console.log(result);
+        if (result.hasOwnProperty("affectedRows")) {
+            return (result["affectedRows"] === 1 &&
+                res.json({ msg: "successfully created a shippment.", success: true }));
+        }
+        return res.json({
+            msg: "failed to create a shippment, please try again.",
+            success: false
+        });
     });
 };
--- src/controllers/v1/address.ts
+++ src/controllers/v1/address.ts
@@ -44,23 +44,26 @@ export const saveAddress = async (req: Request, res: Response) => {
     unit_value,
     value,
   } = req.body;
-  console.log(
-    "charge_code",
-    charge_code,
-    "country",
-    country,
-    "deliver_to",
-    deliver_to
-  );
+  console.log("expected_dispatch", expected_dispatch);
   pool.query(
     `insert into shippments (charge_code,deliver_to,country,residence,detail_address,phone,consignment_weight,product_id,expected_dispatch,contents,unit_value,value) 
          values("${charge_code}","${deliver_to}","${country}","${residence}","${address}","${phone}","${consignment_weight}","${product_classification}","${expected_dispatch}","${contents}","${unit_value}","${value}")`,
     async (err, result, fields) => {
-      if(err){
-        console.log('insert into shippments has errors',err)
+      if (err) {
+        console.log("insert into shippments has errors", err);
         return;
       }
-      console.log(result)
+      console.log(result);
+      if (result.hasOwnProperty("affectedRows")) {
+        return (
+          result["affectedRows"] === 1 &&
+          res.json({ msg: "successfully created a shippment.",success:true })
+        );
+      }
+      return res.json({
+        msg: "failed to create a shippment, please try again.",
+        success:false
+      });
     }
   );
 };
